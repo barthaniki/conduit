b21311af8b8dea72fc35788199ae23b11426e524
Bartha Niki <bar.nik@outlook.com>
2021-07-24 09:30:49 +0200

    modification

--- tests/test_login_logout.py
+++ tests/test_login_logout.py
@@ -5,12 +5,11 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 
 options = Options()
-options.headless = False
+options.headless = True
 
 driver = webdriver.Chrome(executable_path=ChromeDriverManager().install(), options=options)
 
 
-# Manual TC ID: CON-TC004
 def test_login_logout():
     url = "http://localhost:1667/#/"
     driver.get(url)
@@ -19,7 +18,7 @@ def test_login_logout():
     driver.find_element(By.XPATH, '//*[@type="text"]').send_keys("testuser1@example.com")
     driver.find_element(By.XPATH, '//*[@type="password"]').send_keys("Abcd123$")
     driver.find_element(By.TAG_NAME, "button").click()
-    time.sleep(2)
+    time.sleep(3)
     assert driver.find_element(By.XPATH, '//*[@id="app"]/nav/div/ul/li[4]/a').text == "testuser1"
     time.sleep(2)
     driver.find_element(By.XPATH, '//a[@active-class="active"]').click()

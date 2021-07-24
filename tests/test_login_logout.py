4d944dedc23dad37f2f71a2a4441c8f194286055
Bartha Niki <bar.nik@outlook.com>
2021-07-24 23:07:47 +0200

    test file modification

--- tests/test_login_logout.py
+++ tests/test_login_logout.py
@@ -17,17 +17,28 @@ def test_login_logout():
     time.sleep(2)
     # driver.set_window_size(1366, 728)
 
+    # prerequisite: existing test user
+    user_name = "testuser1"
+    user_email = (user_name + "@example.com")
+    password = "Abcd123$"
+
     # test login function
     driver.find_element(By.XPATH, '//a[@href="#/login"]').click()
-    driver.find_element(By.XPATH, '//*[@type="text"]').send_keys("testuser1@example.com")
-    driver.find_element(By.XPATH, '//*[@type="password"]').send_keys("Abcd123$")
+    driver.find_element(By.XPATH, '//*[@type="text"]').send_keys(user_email)
+    driver.find_element(By.XPATH, '//*[@type="password"]').send_keys(password)
     driver.find_element(By.TAG_NAME, "button").click()
     time.sleep(3)
 
     # check login
-    assert driver.find_element(By.XPATH, '//*[@id="app"]/nav/div/ul/li[4]/a').text == "testuser1"
+    assert driver.find_element(By.XPATH, '//*[@id="app"]/nav/div/ul/li[4]/a').text == user_name
     time.sleep(2)
 
     # test logout function
     driver.find_element(By.XPATH, '//a[@active-class="active"]').click()
+
+    # check logout
+    assert driver.find_element(By.XPATH, '//a[@href="#/login"]').is_displayed()
+    time.sleep(2)
+
     driver.close()
+    driver.quit()

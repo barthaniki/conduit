833e67ed987f2a7270c1a80236f27b7b9492c61e
Bartha Niki <bar.nik@outlook.com>
2021-07-24 12:33:37 +0200

    modification

--- tests/test_login_logout.py
+++ tests/test_login_logout.py
@@ -7,19 +7,26 @@ from selenium.webdriver.common.by import By
 options = Options()
 options.headless = True
 
-driver = webdriver.Chrome(executable_path=ChromeDriverManager().install(), options=options)
 
-
+# test login and logout functions
 def test_login_logout():
+    driver = webdriver.Chrome(executable_path=ChromeDriverManager().install(), options=options)
     url = "http://localhost:1667/#/"
     driver.get(url)
-    driver.set_window_size(1366, 728)
+    time.sleep(2)
+    # driver.set_window_size(1366, 728)
+
+    # test login function
     driver.find_element(By.XPATH, '//a[@href="#/login"]').click()
     driver.find_element(By.XPATH, '//*[@type="text"]').send_keys("testuser1@example.com")
     driver.find_element(By.XPATH, '//*[@type="password"]').send_keys("Abcd123$")
     driver.find_element(By.TAG_NAME, "button").click()
     time.sleep(3)
+
+    # check login
     assert driver.find_element(By.XPATH, '//*[@id="app"]/nav/div/ul/li[4]/a').text == "testuser1"
     time.sleep(2)
+
+    # test logout function
     driver.find_element(By.XPATH, '//a[@active-class="active"]').click()
     driver.close()
